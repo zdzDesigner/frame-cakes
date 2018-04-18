9fbc99750e7fd591029b6616cd7e6aed8a645e66
tangshengzhi <tangshengzhi@kashuo.com>
2018-04-18 17:52:49 +0800

    重定向的code 完善    账号初始化重定向

--- src/bns/interceptor.js
+++ src/bns/interceptor.js
@@ -11,9 +11,16 @@ export default function authUpdate(aop){
     return function (response) {
         setToken()
         // console.log(response.data)
-        if( response.data.code == '103108' || response.data.code == '103144' 
-            || response.data.errId == '103108' || response.data.errId == '103144'){
+    
+        if( response.data.code == '103108' || response.data.code == '103144' || response.data.code == '103100'
+            || response.data.errId == '103108' || response.data.errId == '103144' || response.data.errId == '103100'){
             logout()
+        }
+
+        // 该账号未初始化
+        if(response.data.code == '103122'){
+            location.href = `http://${location.host}/console/developer.html#/pc/choose`
+            return
         }
         util.isFunction(aop) && aop(response)
         return response
