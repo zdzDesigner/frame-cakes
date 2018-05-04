b087472e75f8c9a0be95a02b061bec0134958a33
tangshengzhi <tangshengzhi@kashuo.com>
2018-05-04 14:25:08 +0800

    cookei清除的host不对

--- src/bns/jumpErrorPage.js
+++ src/bns/jumpErrorPage.js
@@ -2,7 +2,10 @@ import local from '../model/local'
 import util from '../util'
 
 export default function(){
+	let host = util.parseURL(location.href).host;
+	host = host.replace(/^www\./,'')
+
     local.$cookie.clear('groupKey')
-    local.$cookie.clear('groupKey',{ domain : '.'+ util.parseURL(location.href).host })
+    local.$cookie.clear('groupKey',{ domain : '.'+ host })
     location.href = location.origin + '/console/assets/errors/index.html#/404'
 }
--- src/bns/logout.js
+++ src/bns/logout.js
@@ -3,10 +3,13 @@ import util from '../util'
 import redirect from './redirect.js'
 
 export default function(){
+	let host = util.parseURL(location.href).host;
+	host = host.replace(/^www\./,'')
+
     local.$cookie.clear('TOKEN')
-    local.$cookie.clear('TOKEN',{ domain : '.'+ util.parseURL(location.href).host })
+    local.$cookie.clear('TOKEN',{ domain : '.'+  host})
     local.$cookie.clear('groupKey')
-    local.$cookie.clear('groupKey',{ domain : '.'+ util.parseURL(location.href).host })
+    local.$cookie.clear('groupKey',{ domain : '.'+ host })
     location.href = redirect.getRedirectUrl()    
 }
 
