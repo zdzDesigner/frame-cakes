a7e47b6614753e39d1cd306de8d3af2e6733e55c
unknown <ge.tang@aispeech.com>
2018-05-23 17:45:19 +0800

    提交lang的更下

--- src/util/index.js
+++ src/util/index.js
@@ -5,18 +5,17 @@ import intersection from './intersection.js'
 import parser from './parser.js'
 import di from './di.js'
 import logger from './logger.js'
-
-
+import lang from './lang'
 
 var util = {
     ...functor,
     ...base,
     ...parser,
+    ...lang,
     flatobj,
     intersection,
     di,
     logger
-
 }
 
 export default util
---
+++ src/util/lang.js
@@ -0,0 +1,28 @@
+/**
+ * [hasCnStr 是否存在中文字符串]
+ * @param  {[String]} text  [这是一个测试字符串，just a test。]
+ * @return {[Boolean]}      [true]
+ */
+function hasCnStr (text) {
+  const cn_pattern = new RegExp("[\u4E00-\u9FA5]+")
+  return cn_pattern.test(text)
+}
+
+/**
+ * [textTruncate 按最大长度截断文字]
+ * @param  {[String]} text   [这是一个测试字符串，just a test。]
+ * @param  {[Number]} maxlen [3]
+ * @return {[Boolean]}       [true]
+ */
+function textTruncate (text, maxlen){
+  if (text && text.length > maxlen) {
+    return text.substring(0, maxlen) + '...'
+  } else {
+    return text || ''
+  }
+}
+
+export default  {
+  hasCnStr,
+  textTruncate
+}
