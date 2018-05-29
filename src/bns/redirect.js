ae5838377feb5a2b789812de9c0aa073fce3aa74
unknown <ge.tang@aispeech.com>
2018-05-29 10:16:26 +0800

    更新跳转配置

--- src/bns/redirect.js
+++ src/bns/redirect.js
@@ -1,15 +1,20 @@
 function getRedirectUrl () {
-    const baseUrl =  window.location.host;
-    let redirectUrl = '';
-  
-    if (baseUrl.indexOf('dui.ai') > -1) {
-        let mainUrl = baseUrl.replace(/^(www.|www.data.|data.)/, '')
-        
-        redirectUrl = `http://authentication.${mainUrl}/?service=${encodeURIComponent(location.href)}#/pc/login/commons`
-    } 
-    return redirectUrl
+  const baseUrl =  window.location.host;
+  let redirectUrl = '';
+
+  if (baseUrl.indexOf('dui.ai') > -1) {
+    let mainUrl
+    let _rgls = /(review\.dui\.ai)|(dev\.dui\.ai)|(t\.dui\.ai)|(beta\.dui\.ai)|(dui\.ai)/g.exec(baseUrl)
+    if (!_rgls) {
+      console.log('域名有误，没有匹配到！')
+    } else {
+      mainUrl = _rgls[0]
+    }
+    redirectUrl = `http://authentication.${mainUrl}/?service=${encodeURIComponent(location.href)}#/pc/login/commons`
+  } 
+  return redirectUrl
 }
 
 export default {
-    getRedirectUrl
+  getRedirectUrl
 }
